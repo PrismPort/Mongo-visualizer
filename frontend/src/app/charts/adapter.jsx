16a6837145a644054f1c1f3d0aa9a73dc670ad72
abductedRhino <avocadorable@mailbox.org>
2023-12-19 03:21:37 +0100

    Create chart folder

---
+++ frontend/src/app/charts/adapter.jsx
@@ -0,0 +1,9 @@
+import { AppContext as app_context } from '../context/AppContext';
+import { handleShowDatabases as handle_show_databases } from '../utils/handleShowDatabases';
+import { handleLoadCollections as handle_load_collections } from '../utils/handleLoadCollections';
+
+export {
+    app_context as AppContext,
+    handle_show_databases as handleShowDatabases,
+    handle_load_collections as handleLoadCollections
+};
---
+++ frontend/src/app/charts/components/doughnut.jsx
@@ -0,0 +1,118 @@
+"use client"
+
+import React, { useContext } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import { AppContext, handleShowDatabases, handleLoadCollections } from '../adapter';
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+async function dbs(ctx) {
+  const arr = [];
+  let dbs = await handleShowDatabases();
+  dbs.forEach(async (db) => {
+    let collections = await handleLoadCollections(db);
+    collections?.forEach(async (collection) => {
+      // console.log(`database: ${db} collection: ${collection}`);
+      arr.push(await ctx.handleAnalyzeCollections(localStorage.getItem('mongoURL'), db, collection))
+    });
+  })
+  return arr;
+};
+
+function AExampleDoughnut() {
+  const ctx = useContext(AppContext);
+  console.log(ctx)
+  const boolean_stats = ctx.stats.filter(obj => obj.type === "Boolean");
+  console.log("bool: ", boolean_stats);
+  try {
+    const values = boolean_stats[0].types["Boolean"];
+    const truthy = values.filter(value => value === true);
+    const falsy = values.filter(value => value === false);
+    const data = {
+      labels: [
+        'true',
+        'false'
+      ],
+      datasets: [{
+        data: [truthy.length, falsy.length],
+        backgroundColor: [
+          '#B2DF8A',
+          '#2A5639',
+        ],
+        hoverOffset: 4
+      }]
+    };
+    return (<Doughnut data={data} />);
+  }
+
+  catch (error) {
+    return (<>{error.toString()}</>);
+  }
+}
+
+
+function ExampleDoughnut() {
+  const ctx = useContext(AppContext)
+  console.log("doughnut:", ctx);
+  setTimeout(async function () {
+    console.log(await dbs(ctx));
+  }, 0);
+  const data = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 1000],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+  return (
+    <>
+      <Doughnut data={data} />
+    </>
+  );
+};
+const OtherExampleDoughnut = () => {
+  const data = {
+    datasets: [{
+      data: [{ id: 'Sales', nested: { value: 1500 } }, { id: 'Purchases', nested: { value: 500 } }]
+    }]
+  };
+  const options = {
+    parsing: {
+      key: 'nested.value'
+    }
+  };
+  return (
+    <>
+      <Doughnut data={data} options={options} />
+    </>
+  );
+};
+function TheStatsDisplay() {
+  const ctx = useContext(StatsContext);
+  console.log(ctx)
+  return (
+    <>
+      <textarea value={ctx?.collection}></textarea>
+    </>
+  )
+}
+const MongoDou = () => {
+  return (
+    <>
+      <TheStatsDisplay />
+    </>
+  )
+};
+
+
+export default ExampleDoughnut;
