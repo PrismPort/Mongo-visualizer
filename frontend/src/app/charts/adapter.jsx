fa43a96c40a7f3fd892c5ae23552d6227adc30dd
abductedRhino <avocadorable@mailbox.org>
2023-12-19 03:58:53 +0100

    Edit charts/adapter

--- frontend/src/app/charts/adapter.jsx
+++ frontend/src/app/charts/adapter.jsx
@@ -1,9 +1,11 @@
 import { AppContext as app_context } from '../context/AppContext';
+import { default as app_provider } from '../context/AppContext';
 import { handleShowDatabases as handle_show_databases } from '../utils/handleShowDatabases';
 import { handleLoadCollections as handle_load_collections } from '../utils/handleLoadCollections';
 
 export {
     app_context as AppContext,
+    app_provider as AppProvider,
     handle_show_databases as handleShowDatabases,
     handle_load_collections as handleLoadCollections
 };
