bfe87edbb1b695866eadb0b2405e46b1683b3898
abductedRhino <avocadorable@mailbox.org>
2023-12-25 14:07:49 +0100

    Edit page.js to jush display charts

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,6 +2,8 @@
 
 import LoginForm from "./_components/FormComponents/LoginForm";
 import ClientSessionProvider from "./_context/ClientSessionProvider";
+import { DateBarChart } from './charts/components/date_bar_chart'
+import { NumberBarChart } from './charts/components/number_bar_chart'
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <ClientSessionProvider>
         <LoginForm></LoginForm>
       </ClientSessionProvider>
+      {/* <NumberBarChart /> */}
+      <DateBarChart />
     </div>
   );
 }
