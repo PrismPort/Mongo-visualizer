5902c9ea5e3a0171409dab5562f9c3e1c9d05d76
abductedRhino <avocadorable@mailbox.org>
2024-01-05 23:39:57 +0100

    WIP2

--- frontend/src/app/charts/chart_selector.jsx
+++ frontend/src/app/charts/chart_selector.jsx
@@ -33,20 +33,28 @@ class ChartSelector {
 // Bool (als Chart) 
 // Array, Objects (als Listen)
 
-function StringTest({ specific_data }) {
+function StringChart({ specific_data, path }) {
     const { data } = useContext(AppContext);
-
+    console.log('path', path);
+    console.log(data);
+    const string_data_from_context = data.find((item) => item.path === path);
     return (
         <div className="rounded-lg border-black border-solid border-2 bg-green-500 p-4">
             <p>A string chart:</p>
-            <p>{data.type instanceof Array ? data.type[0] : data.type}</p>
+            <p>{`${JSON.stringify(string_data_from_context)}`}</p>
+            <h1>specific data</h1>
+            <p>{`${JSON.stringify(specific_data)}`}</p>
+            <h1>data</h1>
             <p>{`${JSON.stringify(data, null, 2)}`}</p>
         </div>
     );
 }
 
+function getStringData(data) {
+    return data;
+}
 
-function listChallenge(data) {
+function stringChallenge(data) {
     if (data.type instanceof Array
         && data.type.includes('String')) {
         return true;
@@ -57,13 +65,22 @@ function listChallenge(data) {
     }
 }
 
+class Filter {
+    constructor(path) {
+        this.path = path;
+    }
+    filter(data) {
+        return data.path
+    }
+}
+
 function createSelector() {
     const selector = new ChartSelector();
     // selector.register((data) => (data.type === "String"), StringListChart);
     selector.register((data) => (data === "something"), NumberBarChart);
     // selector.register((data) => (data.type === "Date"), DateBarChart);
     selector.register((data) => (data === "somethings-different"), BooleanDoughnutChart);
-    selector.register((data) => (data.type ? data.type instanceof Array ? data.type.includes('String') : data.type === 'String' : false), StringTest)
+    selector.register(stringChallenge, StringChart)
     // selector.register((data) => (data.type === "Array"), ArrayListChart);
     // selector.register((data) => (data.type === "Object"), ObjectListChart);
     return selector
--- frontend/src/app/charts/components/null_chart.jsx
+++ frontend/src/app/charts/components/null_chart.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Chart from 'chart.js/auto';
 
-export function NullChart({ data }) {
+export function NullChart({ specific_data }) {
     return (
         <div className="rounded-lg border-black border-solid border-2 bg-red-500 p-4">
-            <p>There is no chart defined for this data:</p>
-            <p>{`${JSON.stringify(data.types, null, 2)}`}</p>
+            <p>There is no chart defined for this specific_data:</p>
+            <p>{`${specific_data ? JSON.stringify(specific_data.types, null, 2) : 'undefined'}`}</p>
         </div>
     );
 }
