d0b0d3bbf9d6e3e4214bb15b8cc7daaf3c177339
abductedRhino <avocadorable@mailbox.org>
2024-01-07 04:43:04 +0100

    Edit sendQuery.js

--- frontend/src/app/_utils/sendQuery.js
+++ frontend/src/app/_utils/sendQuery.js
@@ -1,30 +1,16 @@
-const sendQuery = async (query, database, collection) => {
-    // console.log(query);
-    console.log(database, collection);
+export const sendQuery = async (query, database, collection) => {
     const url = `http://localhost:4000/query/${database}/${collection}`;
     try {
-        // setLoading(true); // used for loading screens
         const response = await fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'mongoURL': localStorage.getItem('mongoURL'),
             },
             body: JSON.stringify(query),
         });
         const data = await response.json();
-
-        // show loading overlay
-        // setLoading(true);
-        // hide loading overlay when data is received
-        // setLoading(false);
-        //console.log(data.schema);
-        //console.log(data.collections);
-        updateStats(data.schema);
+        return data;
     } catch (error) {
         console.error(error);
-    } finally {
-        // setLoading(false);  // used for loading screens
     }
-
 };
