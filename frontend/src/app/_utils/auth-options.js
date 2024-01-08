7cb8adceac004eacc12bbf1f64291e585a5eb0f5
abductedRhino <avocadorable@mailbox.org>
2024-01-08 15:34:39 +0100

    Change host.docker.internal to 172.17.0.1

--- frontend/src/app/_utils/auth-options.js
+++ frontend/src/app/_utils/auth-options.js
@@ -14,7 +14,7 @@ export const authOptions = {
           });
 
           const mongoConnectionResponse = await fetch(
-            "http://host.docker.internal:4000/connect-to-mongodb", // Consider updating this URL for production
+            "http://172.17.0.1:4000/connect-to-mongodb", // Consider updating this URL for production
             {
               method: "POST",
               headers: { "Content-Type": "application/json" },
