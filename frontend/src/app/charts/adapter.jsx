07a9668808b1087939eb2c22a2862c0985a8af78
abductedRhino <avocadorable@mailbox.org>
2024-01-15 13:28:53 +0100

    Add TODO

--- frontend/src/app/charts/adapter.jsx
+++ frontend/src/app/charts/adapter.jsx
@@ -3,6 +3,7 @@ import { default as app_provider } from '../_context/AppContext';
 import { handleShowDatabases as handle_show_databases } from '../_utils/handleShowDatabases';
 import { handleLoadCollections as handle_load_collections } from '../_utils/handleLoadCollections';
 
+// TODO: delete this when done with moving files around
 export {
     app_context as AppContext,
     app_provider as AppProvider,
